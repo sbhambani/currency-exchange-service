352bbd14a57cfd81862adf66c772238c05f3ff3c
Sunil Bhambani <sunil_bhambani@persistent.co.in>
2019-02-14 15:02:02 +0530

    Added sleuth and log statements

--- src/main/java/com/learn/microservices/CurrencyExchangeServiceApplication.java
+++ src/main/java/com/learn/microservices/CurrencyExchangeServiceApplication.java
@@ -3,6 +3,9 @@ package com.learn.microservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,5 +15,9 @@ public class CurrencyExchangeServiceApplication {
 		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
 	}
 
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
+	}
 }
 
--- src/main/java/com/learn/microservices/controllers/CurrencyExchangeController.java
+++ src/main/java/com/learn/microservices/controllers/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.learn.microservices.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,8 @@ import com.learn.microservices.repositories.ExchangeValueRepository;
 @RestController
 public class CurrencyExchangeController {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(CurrencyExchangeController.class);
+	
 	@Autowired
 	private Environment environment;
 	
@@ -23,6 +27,7 @@ public class CurrencyExchangeController {
 		//ExchangeValue exchangeValue = new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
 		ExchangeValue exchangeValue = exchangeValueRepository.findByFromAndTo(from, to);
 		exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+		LOGGER.info("{}", exchangeValue);
 		return exchangeValue;
 	}
 }
